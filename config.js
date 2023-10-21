bc3f818d633884846d0da0b3d4e92421b5702e57
KingAnand82 <144103460+KingAnand82@users.noreply.github.com>
2023-10-21 20:01:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['13304082020', 'Dinuwa Official²⁰²³', true],
-  ['13304082020', 'Dinuwa Official²⁰²³', true], 
+  ['13344082020', 'Dinuwa Official²⁰²³', true],
+  ['13344082020', 'Dinuwa Official²⁰²³', true], 
   [''] 
 ] //Number of owners
 
-global.mods = ['13304082020','13304082020'] 
-global.prems = ['13304082020', '13304082020', '94740804536']
+global.mods = ['13344082020','13344082020'] 
+global.prems = ['13344082020', '13344082020', '13344082020']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
